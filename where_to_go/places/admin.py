cafadd27d4a70a92cb1ef28b4f50e86d7626997e
k-mickey <kokmickey@yandex.ru>
2023-11-18 18:03:22 +0300

    Add preview image

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = [
+        "img",
+        "image_preview",
+        "position",
+    ]
+    readonly_fields = ["image_preview"]
+
+    def image_preview(self, obj):
+        return format_html(
+            '<img src="{url}" style="max-height: 200px;"/>',
+            url=obj.img.url
+        )
 
 
 class PlaceAdmin(admin.ModelAdmin):
