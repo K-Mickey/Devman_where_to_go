657be667135367a6684f9ec07b2d390edef68f65
k-mickey <kokmickey@yandex.ru>
2023-12-03 17:12:31 +0300

    rename variables

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -6,40 +6,37 @@ from places.models import Place, Image
 
 
 class Command(BaseCommand):
-    help = 'Load places a link to json file'
+    help = 'Load places from a link to json file'
 
     def add_arguments(self, parser):
         parser.add_argument('link', type=str, action='store')
 
     def handle(self, *args, **kwargs):
         link = kwargs['link']
-        try:
-            response = requests.get(link.strip())
-            response.raise_for_status()
-        except requests.HTTPError as e:
-            self.stdout.write(f'Server returned {e.response.status_code} - {e.response}')
+        response = requests.get(link.strip())
+        response.raise_for_status()
+
+        json_data = response.json()
+        place_model, place_created = Place.objects.get_or_create(
+            title=json_data['title'],
+            defaults={
+                'description_short': json_data['description_short'],
+                'description_long': json_data['description_long'],
+                'lng': json_data['coordinates']['lng'],
+                'lat': json_data['coordinates']['lat']
+            }
+        )
+        if place_created:
+            for position, img in enumerate(json_data['imgs']):
+                img_file = ContentFile(requests.get(img).content)
+                img_name = img.split('/')[-1]
+                image_model, img_created = Image.objects.get_or_create(
+                    img=img_name,
+                    place=place_model,
+                    position=position
+                )
+                if img_created:
+                    image_model.img.save(img_name, img_file, save=True)
+            self.stdout.write(f'Created place {place_model}')
         else:
-            place_data = response.json()
-            place, created = Place.objects.get_or_create(
-                title=place_data['title'],
-                defaults={
-                    'description_short': place_data['description_short'],
-                    'description_long': place_data['description_long'],
-                    'lng': place_data['coordinates']['lng'],
-                    'lat': place_data['coordinates']['lat']
-                }
-            )
-            if created:
-                for num, image in enumerate(place_data['imgs']):
-                    img_file = ContentFile(requests.get(image).content)
-                    filename = image.split('/')[-1]
-                    loc_img, created = Image.objects.get_or_create(
-                        img=filename,
-                        place=place,
-                        position=num
-                    )
-                    if created:
-                        loc_img.img.save(filename, img_file, save=True)
-                self.stdout.write(f'Created place {place}')
-            else:
-                self.stdout.write(f'Place {place} already exists')
+            self.stdout.write(f'Place {place_model} already exists')
