70c44312f3ed78314ef13e60bb72c8430ed5a4c3
k-mickey <kokmickey@yandex.ru>
2023-12-12 22:44:30 +0300

    fix views.py

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
@@ -25,13 +23,13 @@ def home_view(request):
             } for place in Place.objects.all()
         ]
     }
-    context = {'places_data': json.dumps(data)}
+    context = {'places_data': data}
     return render(request, 'index.html', context=context)
 
 
 def place_view(request, place_id):
-    place = get_object_or_404(Place, pk=place_id)
-    place_data = {
+    place = Place.objects.select_related('images').get(pk=place_id)
+    place_serialize = {
         'title': place.title,
         'imgs': [image.img.url for image in place.images.all()],
         'short_description': place.short_description,
@@ -42,7 +40,7 @@ def place_view(request, place_id):
         }
     }
     return JsonResponse(
-        data=place_data,
+        data=place_serialize,
         json_dumps_params={
             'ensure_ascii': False,
             'indent': 2
