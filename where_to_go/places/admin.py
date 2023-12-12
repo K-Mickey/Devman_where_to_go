1b062ea9e7c16e7c8d48cd6b3e17a7e2537fbbc5
k-mickey <kokmickey@yandex.ru>
2023-12-12 22:23:08 +0300

    fix admin.py

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -13,18 +13,19 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     ]
     readonly_fields = ['get_img_preview']
 
-    def get_img_preview(self, obj):
+
+    def get_img_preview(self, image):
         return format_html(
-            '<img src="{url}" style="max-height: 200px;"/>',
-            url=obj.img.url
+            '<img src="{url}" style="max-height: 200px; max-width: 200px"/>',
+            url=image.img.url
         )
 
 
 @admin.register(Place)
 class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = (ImageInline, )
-
     search_fields = ['title']
+    autocomplete_fields = ['images']
 
 
 admin.site.register(Image)
