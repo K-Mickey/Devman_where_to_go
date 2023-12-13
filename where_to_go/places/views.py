a44b7150421a3bf1194f8d56c966044e6915c2b3
k-mickey <kokmickey@yandex.ru>
2023-12-13 15:10:44 +0300

    index.html: rename from places_data to places. views.py: add json.dumps for fix bug. refactor code

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,5 +1,7 @@
+import json
+
 from django.http import JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse
 
 from .models import Place
@@ -7,36 +9,39 @@ from .models import Place
 
 def home_view(request):
     data = {
-        'type': 'FeatureCollection',
-        'features': [
-            {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [place.lng, place.lat]
-                },
-                'properties': {
-                    'title': place.title,
-                    'placeId': place.pk,
-                    'detailsUrl': reverse('place_view', args=[place.pk]),
-                }
-            } for place in Place.objects.all()
-        ]
+        "type": "FeatureCollection",
+        "features": [form_place_json(place) for place in Place.objects.all()]
     }
-    context = {'places_data': data}
+    context = {"places": json.dumps(data)}
     return render(request, 'index.html', context=context)
 
 
+def form_place_json(place):
+    place_serialized = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat]
+        },
+        "properties": {
+            "title": place.title,
+            "placeId": place.pk,
+            "detailsUrl": reverse("place_view", args=[place.pk]),
+        }
+    }
+    return place_serialized
+
+
 def place_view(request, place_id):
-    place = Place.objects.select_related('images').get(pk=place_id)
+    place = Place.objects.prefetch_related('images').get(pk=place_id)
     place_serialize = {
-        'title': place.title,
-        'imgs': [image.img.url for image in place.images.all()],
-        'short_description': place.short_description,
-        'long_description': place.long_description,
-        'coordinates': {
-            'lat': place.lat,
-            'lng': place.lng
+        "title": place.title,
+        "imgs": [image.img.url for image in place.images.all()],
+        "short_description": place.short_description,
+        "long_description": place.long_description,
+        "coordinates": {
+            "lat": place.lat,
+            "lng": place.lng
         }
     }
     return JsonResponse(
